c9e4739c0024951a0501d234175a10fa72c02f4a
Jason Lee <jason.lee@automattic.com>
2026-02-18 01:48:26 -0700

    chore: lint Copilot

--- newspack-theme/js/src/font-loading.js
+++ newspack-theme/js/src/font-loading.js
@@ -1,8 +1,10 @@
 const fontsToLoad = window.newspackFontLoading?.fonts || [];
-Promise.all( fontsToLoad.map( fontName => {
-	const escapedFontName = String( fontName ).replace( /(["\\])/g, '\\$1' );
-	return document.fonts.load( `1rem "${ escapedFontName }"` );
-} ) ).then( res => {
+Promise.all(
+	fontsToLoad.map( fontName => {
+		const escapedFontName = String( fontName ).replace( /(["\\])/g, '\\$1' );
+		return document.fonts.load( `1rem "${ escapedFontName }"` );
+	} )
+).then( res => {
 	if ( res.length === fontsToLoad.length ) {
 		document.body.classList.remove( 'newspack--font-loading' );
 	}
