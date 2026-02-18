b6ab9f91d916aefaa101531466690acf06a31f4a
Jason Lee <jason.lee@automattic.com>
2026-02-18 00:44:45 -0700

    fix: quote font names

--- newspack-theme/js/src/font-loading.js
+++ newspack-theme/js/src/font-loading.js
@@ -1,5 +1,5 @@
 const fontsToLoad = window.newspackFontLoading?.fonts || [];
-Promise.all( fontsToLoad.map( fontName => document.fonts.load( `1rem ${ fontName }` ) ) ).then( res => {
+Promise.all( fontsToLoad.map( fontName => document.fonts.load( `1rem '${ fontName }'` ) ) ).then( res => {
 	if ( res.length === fontsToLoad.length ) {
 		document.body.classList.remove( 'newspack--font-loading' );
 	}
